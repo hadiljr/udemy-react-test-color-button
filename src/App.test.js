5575f583eff554301c3f2bb39667ab0c5d9e2845
Hadil Aparecido Dos Santos Junior <hadil.junior@ambevtech.com.br>
2022-07-27 15:46:04 -0300

    Aula Start Color Button App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link',{name:/learn react/i});
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App/>);
+
+  const element = screen.getByRole('button',{name:'Change to blue'});
+
+  expect(element).toHaveStyle({backgroundColor:'red'});
+
+});
+
+test('button turns blue when clicked',()=>{
+
 });
